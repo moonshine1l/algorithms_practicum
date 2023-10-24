46871ee77c1bff61eeaaf741b11a76cbc5c03bc1
moonshine1l <108587429+moonshine1l@users.noreply.github.com>
2023-10-25 02:38:18 +0300

    Update Fib_binet.java

--- src/ru/moonshine1l/Fib_binet.java
+++ src/ru/moonshine1l/Fib_binet.java
@@ -1,10 +1,10 @@
 package ru.moonshine1l;
 
 public class Fib_binet {
-    public static int fib(int n) {
+    public static long fib(int n) {
         double a = Math.sqrt(5);
         double b = (1 + a)/2;
-        int result = (int) ((Math.pow(b, n) - Math.pow(-b, -n))/a);
+        long result = (long) ((Math.pow(b, n) - Math.pow(-b, -n))/a);
         return result;
     }
 
